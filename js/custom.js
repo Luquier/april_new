a783144c06f588b400ebf625448cf7ff66b0a225
Luquier <nancy.tsr1@gmail.com>
2016-11-16 16:28:01 +0900

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Nancy on 11/10/2016.
+ */
+jQuery(document).ready(function($) {
+
+    $('#myCarousel').carousel({
+        interval: 3000
+    });
+
+
+    $('#myCarousel2').carousel({
+        interval: 3000
+    });
+/*
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click( function(){
+        var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+        var id = parseInt(id);
+        $('#myCarousel').carousel(id);
+    });*/
+
+});
+
+/*
+
+jQuery(document).ready(function($) {
+
+
+
+    //Handles the carousel thumbnails
+    $('[id^=carousel-selector-]').click( function(){
+        var id = this.id.substr(this.id.lastIndexOf("-") + 1);
+        var id = parseInt(id);
+        $('#myCarousel2').carousel(id);
+    });
+
+});*/
